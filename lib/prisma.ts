1dc25d9afc74d1583aced845823c7d7ec855253b
mihalacheD <danutza_dima88@yahoo.com>
2026-02-08 17:44:30 +0200

    Fix: database tls connection issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,7 +8,7 @@ const connectionString = `${process.env.DATABASE_URL}`
 const pool = new Pool({
   connectionString,
   ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: true }
+    ? { rejectUnauthorized: false }
     : { rejectUnauthorized: false }
 })
 
